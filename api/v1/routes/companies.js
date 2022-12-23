311a724099323111c1a7cf499b7e270582be414d
Marius <mulamamarius@gmail.com>
2022-12-24 01:32:09 +0300

    Some small cahnges from put to patchin the request

--- api/v1/routes/companies.js
+++ api/v1/routes/companies.js
@@ -15,10 +15,7 @@ router.get("/:id/jobs", CompanyController.companies_get_jobs);
 
 router.post("/create", CompanyController.create_company);
 
-
-//TO Be changed to the ones commented
-router.get("/modify/:id", CompanyController.modify_company);
-//router.put("/modify/:id", CompanyController.modify_company);
+router.patch("/modify/:id", CompanyController.modify_company);
 
 
 
