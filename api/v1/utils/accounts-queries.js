257243ab6a9c2dc87efa5f08fb9b07f2a05e47a1
Marius <mulamamarius@gmail.com>
2023-02-04 02:08:21 +0300

    Added queries for accounts

--- api/v1/utils/accounts-queries.js
+++ api/v1/utils/accounts-queries.js
@@ -1,9 +1,12 @@
-const createUserWithPass = "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, crypt($4, gen_salt('md5')))";
-const createUserWithoutPass = "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3)";
+const createUserWithPass = "INSERT INTO users (email, password) VALUES ($1, crypt($2, gen_salt('md5')))";
+const changePassword = "UPDATE users SET password = crypt('$2', gen_salt('md5')) where email = $1";
+const createUserWithoutPass = "INSERT INTO users (first_name, last_name, email) VALUES ($1, $2, $3)";
 const login = "SELECT *, CONCAT(first_name,' ', last_name) AS full_name FROM users WHERE email = $1 AND password = crypt($2, password)";
+const checkIfExists = "SELECT * FROM users WHERE email = $1";
 
 module.exports = {
     createUserWithPass,
     createUserWithoutPass,
-    login
+    login,
+    checkIfExists
 }
