ae8e9ab2e951d699c115f73d4ab1b8efdae426f0
Marius <mulamamarius@gmail.com>
2023-02-04 02:06:13 +0300

    feat: Added updating of password feature and included jwt on login and some checks when signing up

--- api/v1/controllers/accounts.js
+++ api/v1/controllers/accounts.js
@@ -1,67 +1,117 @@
+const jwt = require('jsonwebtoken');
+
 const queries = require("../utils/accounts-queries");
 const pool = require("../utils/db-connection");
+const { checksignup } = require("../utils/do-checks");
 
-const login = (req,res)=>{
-    const email = req.body.email;
-    const password = req.body.password;
+const login = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-    pool.query(queries.login,[email,password],(error,result)=>{
-        if (error){
-            return res.status(500).json({
-                message:"An Error occured",
-                error:error
-            })
+  pool.query(queries.login, [email, password], (error, result) => {
+    if (error) {
+      return res.status(500).json({
+        message: "An Error occured",
+        error: error,
+      });
+    }
+
+    if (result.rows.length === 1) {
+      const token = generateJWT(result.rows[0])
+    
+      //console.log(result);
+      res.status(200).json({
+        message: "Succesful Login",
+        result: result.rows[0],
+        token: token
+      });
+    }
+
+    if (result.rows.length != 1) {
+      return res.status(403).json({
+        message: "Authentication Failed",
+      });
+    }
+  });
+};
+
+const signup = (req, res) => {
+  const email = String(req.body.email).toLowerCase();
+  const password = String(req.body.password);
+
+  const issues = checksignup(email, password);
+
+  if (issues.length > 0) {
+    return res.status(409).json({
+      message: issues,
+    });
+  }
+
+  pool.query(queries.createUserWithPass, [email, password], (error, result) => {
+    if (error) {
+      return res.status(500).json({
+        message: "An error Ocuured",
+        error: error,
+      });
+    }
+
+    if (result) {
+      pool.query(queries.checkIfExists, [email], (error, result) => {
+        if (error) {
+          return res.status(200);
         }
 
-        if(result.rows.length === 1){
-            console.log(result)
-            res.status(200).json({
-                message:"Succesful Login",
-                result:result.rows[0]
-            })
+        if (result) {
+          return res.status(201).json({
+            message: "User Created",
+            result: result.rowCount,
+          });
         }
+      });
+    }
+  });
+};
 
-        if (result.rows.length != 1){
-            return res.status(403).json({
-                message:"Authentication Failed"
-            })
-        }
-    })
+const changePassword = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-}
+  pool.query(queries.changePassword, [email, password], (error, result) => {
+    if (error) {
+      return res.status(409).json({
+        message: "Error Occured while updating password try again",
+        error: error,
+      });
+    }
 
+    if (result) {
+      return res.status(200).json({
+        message: "PAssword was updated succesfully",
+        result: result.rowCount,
+      });
+    }
+  });
+};
 
-const signup =  (req,res)=>{
-    const first_name = req.body.first_name
-    const last_name  = req.body.last_name
-    const email = req.body.email
-    const password = req.body.password
+function generateJWT(data){
+  const token = jwt.sign(
+    {
+      email: data.email,
+      identifier: data.uuid,
+      verified: data.verified,
+      fullname: data.full_name
+    },
+    process.env.JWT_KEY,
+    {
+      expiresIn: "720h",
+    } 
+    );
 
-    //console.log(req.body)
-
-
-    pool.query(queries.createUserWithPass,[first_name, last_name, email, password],(error,result)=>{
-        if(error){
-            return res.status(500).json({
-                message:"An error Ocuured",
-                error:error            
-            })
-        }
-
-        if (result){
-            return res.status(200).json({
-                message:"User Created",
-                result:result.rowCount
-            })
-        }
-
-
-        
-    })
+    return token
 }
 
 module.exports = {
-    login,
-    signup,
-
-}
+  login,
+  signup,
+  changePassword,
+};
