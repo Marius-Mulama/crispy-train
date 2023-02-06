ea0ec5be2bf583a1a6f363d582d33c2817069b22
Marius <mulamamarius@gmail.com>
2023-02-07 00:02:46 +0300

    feat: Addition of authentication checking middleware

--- api/v1/utils/check-auth.js
+++ api/v1/utils/check-auth.js
@@ -4,19 +4,20 @@ const jwt = require('jsonwebtoken');
 module.exports = (req,res,next)=>{
     try{
         const token = req.headers.authorization.split(" ")[1];
-        console.log(token);
+        //console.log(token);
         const decoded = jwt.verify(token, process.env.JWT_KEY);
         req.userData = decoded;
-        console.log(req.userData);
+        //console.log(req.userData);
         next();
 
     }catch(error){
         //console.log(error);
         return res.status(401).json({
-            Message: "auth Failed"
+            Message: "auth Failed",
+            error:error
         });
     }
     
-    next();
+   // next();
     
 }
