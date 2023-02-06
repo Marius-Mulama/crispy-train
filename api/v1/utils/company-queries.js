d3c0fb8d475a17e9030d088653f0e645b5e847ca
Marius <mulamamarius@gmail.com>
2023-02-07 00:02:00 +0300

    change in SQL statements to support the slug instead of exposing uuid

--- api/v1/utils/company-queries.js
+++ api/v1/utils/company-queries.js
@@ -1,7 +1,7 @@
 //Holds all the SQL queries for everything involving companies in the api
 
 const viewAllCompanies = "SELECT * FROM companies LIMIT 20";
-const viewSingleCompany = "SELECT * FROM companies WHERE uuid = $1"
+const viewSingleCompany = "SELECT * FROM companies WHERE slug = $1"
 const createCompany = "INSERT INTO companies(name, description, location, website) VALUES ($1, $2, $3, $4)";
 
 
