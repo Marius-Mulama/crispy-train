e709014d582fac71c34e6f106ff1dc75c7d6966f
Marius <mulamamarius@gmail.com>
2023-03-25 16:48:30 +0300

    added the routing file

--- api/v1/routes/accounts.js
+++ api/v1/routes/accounts.js
@@ -6,6 +6,8 @@ const router = express.Router()
 const AccountsController = require("../controllers/accounts");
 const checkAuth = require("../utils/checks/check-auth");
 
+const imageUpload = require("../utils/uploads/profileImages.js")
+
 
 router.get("/", (req,res)=>{
     res.status(200).json({
@@ -25,6 +27,10 @@ router.patch("/experience",checkAuth, AccountsController.updateExperience)
 router.get("/myprofile",checkAuth, AccountsController.getMyProfile);
 
 router.patch("/roles", checkAuth,AccountsController.updateRole);
+
+
+//TODO Find a fix for this
+router.patch("/upload",imageUpload, AccountsController.addProfilePic);
 
 
 //router.get("experince", checkAuth, AccountsController.viewExperience)
