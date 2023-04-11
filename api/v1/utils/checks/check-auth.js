667ae667f66736bfa671f23ddf5bf2aec4613994
Marius <mulamamarius@gmail.com>
2023-04-12 02:54:16 +0300

    Add checkauth

--- api/v1/utils/checks/check-auth.js
+++ api/v1/utils/checks/check-auth.js
@@ -1,5 +1,23 @@
 const jwt = require('jsonwebtoken');
 
+const checkExtranet = (req,res,next)=>{
+    try{
+        const token = req.headers.authorization.split(" ")[1];
+        //console.log(token);
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        req.userData = decoded;
+        //console.log(req.userData);
+        next();
+
+    }catch(error){
+        //console.log(error);
+        return res.status(401).json({
+            Message: "auth Failed",
+            error:error
+        });
+    }
+}
+
 
 module.exports = (req,res,next)=>{
     try{
@@ -21,3 +39,16 @@ module.exports = (req,res,next)=>{
    // next();
     
 }
+
+
+function isAdminus(webtoken){
+    let token = webtoken;
+    let decoded = jwt.verify(token, process.env.JWT_KEY);
+  
+    if(decoded.role === "User" || decoded.role ==='Company' ){
+      return false;
+    }
+  
+    return true;
+  
+  }
